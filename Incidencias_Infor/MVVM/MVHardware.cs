5a577609d55dcc436de33b93d3d6e07b2729aba7
Felipin Murcielago <Felipepereztevar@gmail.com>
2022-03-14 08:45:02 +0100

    Hemos creado los MV de hardware y software, y los hemos preparado para funcionar

---
+++ Incidencias_Infor/MVVM/MVHardware.cs
@@ -0,0 +1,37 @@
+﻿using Incidencias_Infor.Backend.Modelo;
+using Incidencias_Infor.Backend.Servicio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incidencias_Infor.MVVM
+{
+    class MVHardware : MVBaseCRUD<hardware>
+    {
+        private incidenciasEntities inciEnt;
+        private HardwareServicio hardServ;
+        private hardware hard;
+
+        public MVHardware(incidenciasEntities ent)
+        {
+            inciEnt = ent;
+            inicializa();
+
+            servicio = hardServ;
+        }
+
+        private void inicializa()
+        {
+            hardServ = new HardwareServicio(inciEnt);
+            hard = new hardware();
+        }
+
+        public hardware hardNuevo { get { return hard; } set { hard = value;NotifyPropertyChanged(nameof(hardNuevo));} }
+
+        public bool guarda { get { return add(hardNuevo); } }
+        public bool edita { get { return update(hardNuevo); } }
+        public bool borrar { get{ return delete(hardNuevo); } }
+    }
+}
---
+++ Incidencias_Infor/MVVM/MVSoftware.cs
@@ -0,0 +1,38 @@
+﻿using Incidencias_Infor.Backend.Modelo;
+using Incidencias_Infor.Backend.Servicio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incidencias_Infor.MVVM
+{
+    class MVSoftware : MVBaseCRUD<software>
+    {
+        
+        private SoftwareServicio softServ;
+        private incidenciasEntities inciEnt;
+        private software soft;
+
+        public MVSoftware(incidenciasEntities ent)
+        {
+            inciEnt = ent;
+            inicializa();
+
+            servicio = softServ;
+        }
+
+        private void inicializa()
+        {
+            softServ = new SoftwareServicio(inciEnt);
+            soft = new software();
+        }
+
+        public software softNuevo { get { return soft; } set { soft = value; NotifyPropertyChanged(nameof(softNuevo)); } }
+
+        public bool guarda { get { return add(softNuevo); } }
+        public bool edita { get { return update(softNuevo); } }
+        public bool borrar { get { return delete(softNuevo); } }
+    }
+}
